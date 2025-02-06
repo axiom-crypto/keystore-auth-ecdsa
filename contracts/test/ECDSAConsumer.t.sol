cd82dc19b2b15ae5c3e2ad75fc189d566fb0512a
Hrik Bhowal <23041116+HrikB@users.noreply.github.com>
2025-02-05 20:36:53 -0500

    feat: add ECDSAConsumer and simple test

---
+++ contracts/test/ECDSAConsumer.t.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import { Test } from "forge-std/Test.sol";
+
+import { ECDSAConsumer } from "../src/ECSDAConsumer.sol";
+
+contract ECDSAConsumerTest is Test {
+    ECDSAConsumer public consumer;
+    address[] public allowedSignersList;
+    uint256 privKey1;
+
+    function setUp() public {
+        consumer = new ECDSAConsumer();
+
+        (address address1, uint256 _privKey1) = makeAddrAndKey("address1");
+        (address address2,) = makeAddrAndKey("address2");
+
+        privKey1 = _privKey1;
+
+        allowedSignersList.push(address1);
+        allowedSignersList.push(address2);
+    }
+
+    function test_simple() public view {
+        // codehash doesn't matter here
+        bytes memory keyData =
+            _constructKeyData({ codehash: bytes32(0), requiredSigners: 1, _allowedSignersList: allowedSignersList });
+
+        bytes32 msgHash = keccak256("msgHash");
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privKey1, msgHash);
+
+        bytes memory signature = abi.encodePacked(r, s, v);
+
+        consumer.consumeKeyData({ keyData: keyData, walletSignatures: signature, userOpHash: msgHash });
+    }
+
+    function _constructKeyData(bytes32 codehash, uint256 requiredSigners, address[] memory _allowedSignersList)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        require(
+            requiredSigners <= _allowedSignersList.length,
+            "Required signers must be less than or equal to allowed signers list length"
+        );
+
+        return abi.encodePacked(bytes1(0x00), abi.encode(codehash, requiredSigners, _allowedSignersList));
+    }
+}
